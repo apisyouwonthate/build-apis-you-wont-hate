e96b573b17c23e4ba20a1e7a19007d0e19c4338b
Phil Sturgeon <email@philsturgeon.co.uk>
2013-12-10 19:17:38 -0500

    Forgot to typecast place.

--- fractal-demo/app/src/App/Transformer/PlaceTransformer.php
+++ fractal-demo/app/src/App/Transformer/PlaceTransformer.php
@@ -19,13 +19,13 @@ class PlaceTransformer extends TransformerAbstract
         return [
             'id'           => (int) $place->id,
             'name'         => $place->name,
-            'lat'          => $place->lat,
-            'lon'          => $place->lon,
+            'lat'          => (float) $place->lat,
+            'lon'          => (float) $place->lon,
             'address1'     => $place->address1,
             'address2'     => $place->address2,
             'city'         => $place->city,
             'state'        => $place->state,
-            'zip'          => $place->zip,
+            'zip'          => (float) $place->zip,
             'website'      => $place->website,
             'phone'        => $place->phone,
         ];
