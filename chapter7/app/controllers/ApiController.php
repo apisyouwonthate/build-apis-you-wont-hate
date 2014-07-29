fe9e237aebd3360331d78ed8f876d9067d373e60
Iván Sanchez <sangar1982@gmail.com>
2014-07-29 15:57:32 +0200

    Bad Method Name
    Change method name responseWithError because this not exist. The correct name is respondWithError

--- chapter7/app/controllers/ApiController.php
+++ chapter7/app/controllers/ApiController.php
@@ -97,7 +97,7 @@ class ApiController extends Controller
      */
     public function errorForbidden($message = 'Forbidden')
     {
-        return $this->setStatusCode(403)->responseWithError($message, self::CODE_FORBIDDEN);
+        return $this->setStatusCode(403)->respondWithError($message, self::CODE_FORBIDDEN);
     }
 
     /**
@@ -107,7 +107,7 @@ class ApiController extends Controller
      */
     public function errorInternalError($message = 'Internal Error')
     {
-        return $this->setStatusCode(500)->responseWithError($message, self::CODE_INTERNAL_ERROR);
+        return $this->setStatusCode(500)->respondWithError($message, self::CODE_INTERNAL_ERROR);
     }
     
     /**
@@ -117,7 +117,7 @@ class ApiController extends Controller
      */
     public function errorNotFound($message = 'Resource Not Found')
     {
-        return $this->setStatusCode(404)->responseWithError($message, self::CODE_NOT_FOUND);
+        return $this->setStatusCode(404)->respondWithError($message, self::CODE_NOT_FOUND);
     }
 
     /**
@@ -127,7 +127,7 @@ class ApiController extends Controller
      */
     public function errorUnauthorized($message = 'Unauthorized')
     {
-        return $this->setStatusCode(401)->responseWithError($message, self::CODE_UNAUTHORIZED);
+        return $this->setStatusCode(401)->respondWithError($message, self::CODE_UNAUTHORIZED);
     }
 
     /**
@@ -137,6 +137,6 @@ class ApiController extends Controller
      */
     public function errorWrongArgs($message = 'Wrong Arguments')
     {
-        return $this->setStatusCode(400)->responseWithError($message, self::CODE_WRONG_ARGS);
+        return $this->setStatusCode(400)->respondWithError($message, self::CODE_WRONG_ARGS);
     }
 }
